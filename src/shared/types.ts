8fc615dfbf74e2cc72665ebb7fd8493a003e4f15
Sandeep Singh <106710997+singhlancer@users.noreply.github.com>
2023-01-16 10:14:01 +0530

    Add files via upload

---
+++ src/shared/types.ts
@@ -0,0 +1,18 @@
+
+export enum SelectedPage {
+    Home = "home",
+    Benefits = "benefits",
+    OurClasses = "ourclasses",
+    ContactUs = "contactus"
+  }
+
+  export interface BenefitsType {
+    icon: JSX.Element;
+    title:string;
+    description: string;
+  }
+  export interface ClassType {
+    name: string;
+    description?: string;
+    image: string;
+  }
